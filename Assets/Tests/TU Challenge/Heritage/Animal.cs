6ab4639ddc771b3193211017f40ccbaec7e25023
Kévin Roussel <9155865+KevinRoussel@users.noreply.github.com>
2023-11-06 13:38:49 +0100

    TU

---
+++ Assets/Tests/TU Challenge/Heritage/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TU_Challenge.Heritage
+{
+
+    public class Animal
+    {
+
+        public event Action OnDie;
+
+        public bool IsAlive { get; set; }
+        public bool Pattes { get; set; }
+        public bool Name { get; set; }
+
+        public bool Crier()
+        {
+            throw new NotImplementedException();
+        }
+        public void Die()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Tests/TU Challenge/Pathfinding/Pathfinding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TU_Challenge
+{
+    public class Pathfinding
+    {
+        public Pathfinding(string map1)
+        {
+        }
+
+        public char[,] Grid { get; internal set; }
+
+        internal Path BreadthFirstSearch(Vector2 start, Vector2 destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal char GetCoord(Vector2 el)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<char> GetNeighboors(Vector2 vector2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<char> GetNeighboors(Vector2 vector2, List<Vector2> exclude)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool IsOutOfBound(Vector2 vector2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
